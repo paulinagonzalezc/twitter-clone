85dbbed61305cc771e8d30c5084a0b6ec0b4a8d0
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-08-01 10:08:31 -0500

    Added sidebar options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./Sidebar.css";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* Twitter icon */}
       <TwitterIcon />
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Mail" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmark" />
+      <SidebarOption Icon={ListAltIcon} text="List" />
+      <SidebarOption Icon={PermIdentityIcon} text="Identity" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
       {/* SidebarOption */}
     </div>
   );
