2f1a8df13a78c3775d95299803198f42f9db92c1
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-07-31 23:10:06 -0500

    Started with the sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    //BEM
+    <div className="app">
       <h1>Hey Clever Programmers, let's build a twitter clone</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+    </div>
+  );
+}
+
+export default Sidebar;
