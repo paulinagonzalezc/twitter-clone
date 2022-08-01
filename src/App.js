dfdbdbf9ff125c3a967cba874c2b5ead425dce32
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-07-31 19:23:05 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Clever Programmers, let's build a twitter clone</h1>
+    </div>
+  );
+}
+
+export default App;
