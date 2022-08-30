25d3990097e5153ff706b5c298766b208876c61c
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-08-30 17:44:09 -0400

    FIrst part widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets_input">
+        <SearchIcon className="widgets_searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+      </div>
     </div>
   );
 }
