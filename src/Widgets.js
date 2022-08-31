f6e442c2e0cdb17bde618e9388b575eadeb6d362
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-08-31 16:09:24 -0500

    Added features to widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -17,11 +17,15 @@ function Widgets() {
 
       <div className="widgets_widgetContainer">
         <h2>What's happening</h2>
-        <TwitterTweetEmbed tweetId={""} />
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
         <TwitterTimelineEmbed
           sourceType="profile"
           screenName="paulinagzzc"
           options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
         />
       </div>
     </div>
