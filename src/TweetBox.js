58f05a4035a75404a7e1c017118f4e7987a82ab8
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-09-01 12:12:22 -0500

    Added Tweetbox functionality

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@mui/material";
+import { db } from "./firebase";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Paulina Gonzalez",
+      username: "paulinagzzc",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://ih1.redbubble.net/image.1343394098.5639/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox_input">
           <Avatar src="https://ih1.redbubble.net/image.1343394098.5639/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg" />
-          <input placeholder="What's happening?" type="text"></input>
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          ></input>
         </div>
         <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
           className="tweetBox_imageInput"
           placeholder="Enter image URL"
           type="text"
         ></input>
-        <Button className="tweetBox_tweetButton">Tweet</Button>
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox_tweetButton"
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
